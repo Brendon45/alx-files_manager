1e38d006db9a9d68a2a991a2a47f55d254a2b5c5
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-04 22:58:58 +0200

    Modified Task 2. First API.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import express from 'express';
-import router from './routes/index';
+import router from "./routes/index";
+const express = require("express");
 
-const port = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use("/", router);
 
-app.listen(port, () => {
-  console.log(`Server running on ${port}`);
+// start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
